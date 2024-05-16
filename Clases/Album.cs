af0249ffdc02473d1f587af35e8a67bc47c836fb
RonyC7 <carpioalvaradorony@mesoamericana.edu.gt>
2024-05-16 08:55:18 -0600

    Creacion de las clases Cancion y Album

---
+++ Clases/Album.cs
@@ -0,0 +1,25 @@
+﻿public class Album
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public List<Cancion> Canciones { get; set; }
+    public DateTime FechaPublicacion { get; set; }
+
+    public Album(string titulo, string artista, DateTime fechaPublicacion)
+    {
+        Titulo = titulo;
+        Artista = artista;
+        FechaPublicacion = fechaPublicacion;
+        Canciones = new List<Cancion>();
+    }
+
+    public void AgregarCancion(Cancion cancion)
+    {
+        Canciones.Add(cancion);
+    }
+
+    public override string ToString()
+    {
+        return $"{Titulo} - {Artista} ({FechaPublicacion.ToShortDateString()})";
+    }
+}
---
+++ Clases/Cancion.cs
@@ -0,0 +1,20 @@
+﻿public class Cancion
+{
+    public string Nombre { get; set; }
+    public string Artista { get; set; }
+    public TimeSpan Duracion { get; set; }
+    public string Archivo { get; set; }
+
+    public Cancion(string nombre, string artista, TimeSpan duracion, string archivo)
+    {
+        Nombre = nombre;
+        Artista = artista;
+        Duracion = duracion;
+        Archivo = archivo;
+    }
+
+    public override string ToString()
+    {
+        return $"{Nombre} - {Artista} ({Duracion})";
+    }
+}
